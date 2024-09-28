3bc5d8774d07ab472fabb4014deae49cc6f4fa06
Sheikh Haris Zahid <sheikh.haris.zahid@hotmail.com>
2024-09-28 20:32:16 +0500

    feat [c1][w2]: compute of linear regression, cost, and gradient descent vectorized

--- utils/regression.py
+++ utils/regression.py
@@ -1,10 +1,14 @@
 import math
 
-import numpy as np
+import numpy as np, copy
 
 
 def predict(w, b, x):
     return w * x + b
+
+
+def predict_vectorized(w, b, x):
+    return np.dot(x, w) + b
 
 
 def compute_regression(w, b, x):
@@ -13,6 +17,14 @@ def compute_regression(w, b, x):
     for i in range(m):
         f_wb[i] = predict(w, b, x[i])
 
+    return f_wb
+
+
+def compute_regression_vectorized(w, b, x):
+    m = x.shape[0]
+    f_wb = np.zeros(m)
+    for i in range(m):
+        f_wb[i] = predict_vectorized(w, b, x[i])
     return f_wb
 
 
@@ -27,6 +39,16 @@ def compute_cost(x, y, w, b):
     return total_cost
 
 
+def compute_cost_vectorized(x, y, w, b):
+    m = x.shape[0]
+    cost = 0.0
+    for i in range(m):
+        f_wb_i = predict_vectorized(w, b, x[i])  # (n,)(n,) = scalar (see np.dot)
+        cost = cost + (f_wb_i - y[i]) ** 2  # scalar
+    cost = cost / (2 * m)  # scalar
+    return cost
+
+
 def compute_gradient(x, y, w, b):
     m = x.shape[0]
     dj_dw = 0
@@ -38,6 +60,26 @@ def compute_gradient(x, y, w, b):
         dj_db_i = f_wb - y[i]
         dj_db += dj_db_i
         dj_dw += dj_dw_i
+    dj_dw = dj_dw / m
+    dj_db = dj_db / m
+
+    return dj_dw, dj_db
+
+
+def compute_gradient_vectorized(x, y, w, b):
+    m, n = x.shape
+    dj_dw = np.zeros((n,))
+    dj_db = 0
+
+    for i in range(m):
+        # Compute the prediction
+        f_wb = predict_vectorized(w, b, x[i])
+        # Compute the cost
+        cost = f_wb - y[i]
+        # Compute the gradient
+        for j in range(n):
+            dj_dw[j] += cost * x[i, j]
+        dj_db += cost
     dj_dw = dj_dw / m
     dj_db = dj_db / m
 
@@ -90,3 +132,44 @@ def gradient_descent(x, y, w_in, b_in, alpha, num_iters):
             )
 
     return w, b, J_history, p_history  # return w and J,w history for graphing
+
+
+def gradient_descent_vectorized(x, y, w_in, b_in, alpha, num_iters):
+    """
+    Performs gradient descent to fit w,b. Updates w,b by taking
+    num_iters gradient steps with learning rate alpha
+
+    Args:
+        x (ndarray (m,))  : Data, m examples
+        y (ndarray (m,))  : target values
+        w_in,b_in (scalar): initial values of model parameters
+        alpha (float):     Learning rate
+        num_iters (int):   number of iterations to run gradient descent
+
+    Returns:
+        w (scalar): Updated value of parameter after running gradient descent
+        b (scalar): Updated value of parameter after running gradient descent
+        J_history (List): History of cost values
+    """
+
+    # An array to store cost J and w's at each iteration primarily for graphing later
+    J_history = []
+    b = b_in
+    w = copy.deepcopy(w_in)
+
+    for i in range(num_iters):
+        # Calculate the gradient and update the parameters using gradient_function
+        dj_dw, dj_db = compute_gradient_vectorized(x, y, w, b)
+
+        # Update Parameters using equation (3) above
+        b = b - alpha * dj_db
+        w = w - alpha * dj_dw
+
+        # Save cost J at each iteration
+        if i < 100000:  # prevent resource exhaustion
+            J_history.append(compute_cost_vectorized(x, y, w, b))
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i % math.ceil(num_iters / 10) == 0:
+            print(f"Iteration {i:4}: Cost {J_history[-1]:0.2e} ")
+
+    return w, b, J_history  # return final w,b and J history for graphing
